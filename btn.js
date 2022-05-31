96415b1197de9c8a4465b529b1e23bd70a033269
lhs0698 <104623873+lhs0698@users.noreply.github.com>
2022-05-31 18:47:26 +0900

    first commit

---
+++ btn.js
@@ -0,0 +1,19 @@
+const click = document.querySelector(".push_button");
+const clicks = document.querySelector(".stop_click");
+
+
+
+setInterval(function changecolor_click(){
+    document.bgColor = "aqua";
+},1000)
+
+setInterval(function changecolor_click(){
+    document.bgColor = "pink";
+
+},1000)
+
+function stop_click() {
+    
+}
+
+
