2b4f3a66d3a7d534859cabb0ca5cacc57bb9bb4a
lhs0698 <yohs0698@naver.com>
2022-06-02 02:29:44 +0900

    clearinterval

--- btn.js
+++ btn.js
@@ -2,18 +2,24 @@ const click = document.querySelector(".push_button");
 const clicks = document.querySelector(".stop_click");
 
 
+function changecolor_click(){
 
-setInterval(function changecolor_click(){
-    document.bgColor = "aqua";
-},1000)
 
-setInterval(function changecolor_click(){
-    document.bgColor = "pink";
+    setInterval(function changecolor_click(){
+        document.bgColor = "aqua";
+        console.log("blue");
+    },500)
+        setInterval(function changecolor_click(){
+            document.bgColor = "pink";
+            console.log("red");   
+    },600)
+    
+}    
 
-},1000)
 
 function stop_click() {
-    
+    clearInterval();
+    alert("dd");
 }
 
 
