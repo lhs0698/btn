2ae47316eb5669228cf46487e0c3a38adca1855b
lhs0698 <104623873+lhs0698@users.noreply.github.com>
2022-06-02 21:13:07 +0900

    ddd

--- btn.js
+++ btn.js
@@ -1,19 +1,45 @@
-const click = document.querySelector(".push_button");
-const clicks = document.querySelector(".stop_click");
+// const click = document.querySelector(".push_button");
+// const clicks = document.querySelector(".stop_button");
 
 
 
-setInterval(function changecolor_click(){
-    document.bgColor = "aqua";
-},1000)
 
-setInterval(function changecolor_click(){
-    document.bgColor = "pink";
 
-},1000)
 
-function stop_click() {
-    
+
+var inter
+var inter2
+
+function changecolor_click(){
+
+    inter = setInterval(function color(){
+        document.body.style.backgroundColor = "aqua"
+    },500)
+    inter2 = setInterval(function color(){
+        document.body.style.backgroundColor = "pink"
+    },1000)
+
+    console.log(changecolor_click)
+} 
+
+function stop_click(){
+    clearInterval(inter)
+    clearInterval(inter2)
+    document.body.style.backgroundColor = "white"
 }
 
 
+// function stop_click(){
+//     clearInterval(inter)
+//     clearInterval(inter2)
+// }
+
+
+
+// setInterval(function changecolor_click(){
+//     document.bgColor = "aqua";
+// },500)
+
+// setInterval(function changecolor_click(){
+//     document.bgColor = "pink";
+// },700)
