5ad0977175939b45be059f5369392d0ac68b24ee
lhs0698 <104623873+lhs0698@users.noreply.github.com>
2022-06-02 21:24:31 +0900

    dbugst

--- btn.js
+++ btn.js
@@ -2,10 +2,8 @@
 // const clicks = document.querySelector(".stop_button");
 
 
-function changecolor_click(){
 
 
-<<<<<<< HEAD
 
 
 
@@ -28,23 +26,6 @@ function stop_click(){
     clearInterval(inter)
     clearInterval(inter2)
     document.body.style.backgroundColor = "white"
-=======
-    setInterval(function changecolor_click(){
-        document.bgColor = "aqua";
-        console.log("blue");
-    },500)
-        setInterval(function changecolor_click(){
-            document.bgColor = "pink";
-            console.log("red");   
-    },600)
-    
-}    
-
-
-function stop_click() {
-    clearInterval();
-    alert("dd");
->>>>>>> 2b4f3a66d3a7d534859cabb0ca5cacc57bb9bb4a
 }
 
 
