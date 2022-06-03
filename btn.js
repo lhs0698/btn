753b71c226366e882733f554c99a364048a2d926
lhs0698 <yohs0698@naver.com>
2022-06-04 03:29:37 +0900

    todaybutton add

--- btn.js
+++ btn.js
@@ -1,30 +1,60 @@
-// const click = document.querySelector(".push_button");
-// const clicks = document.querySelector(".stop_button");
+let inter;
+let inter2;
+// 전역변수
 
-var inter;
-var inter2;
+function changecolor_click(){
 
-function changecolor_click(a, btn){
-    console.log(a);
-    console.log(btn);
+    inter = setInterval(function color(){
+        document.body.style.backgroundColor = "aqua"
+    },500);
+    inter2 = setInterval(function color(){
+        document.body.style.backgroundColor = "pink"
+    },700);
 
-    // inter = setInterval(function color(){
-    //     document.body.style.backgroundColor = "aqua"
-    // },500);
-    // inter2 = setInterval(function color(){
-    //     document.body.style.backgroundColor = "pink"
-    // },700);
+    // let textInput = document.getElementById('setColor');
+    // let colorValue = textInput.value;
 
-    let textInput = document.getElementById('setColor');
-    let colorValue = textInput.value;
-
-    document.body.style.backgroundColor = colorValue;
-
-
+    // document.body.style.backgroundColor = colorValue;
+    // push 클릭 시 배경색 변경
 } 
+// push 버튼 이벤트
+
+function getInter(){
+    return [inter, inter2];
+}
 
 function stop_click(){
-    clearInterval(inter)
-    clearInterval(inter2)
+    let inters = getInter();
+
+    clearInterval(inters[0]);
+    clearInterval(inters[1]);
     document.body.style.backgroundColor = "white"
+}
+// stop 버튼 이벤트
+
+
+let timer;
+let curtime;
+
+function today_click(){
+    const time = new Date();
+    const year = time.getFullYear();
+    const month = time.getMonth()+1;
+    const date = time.getDate();
+    const hour = time.getHours();
+    const minute = time.getMinutes();
+    const second = time.getSeconds();
+
+    timer = [year,month,date,hour,minute,second];
+    
+    // alert(timer);
+    // console.log(timer[2]);
+
+    let curtime = timer[3];
+    console.log(curtime);
+    if (curtime >= 3){
+        document.body.style.backgroundColor = "yellow"
+    }else {
+        document.body.style.backgroundColor = "green"
+    }
 }
