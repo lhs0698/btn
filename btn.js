e2ea15ec79601bcd6c476d4ec8450a95aab8bdf5
joohyun jo <jjh@alt-a.net>
2022-06-03 17:47:11 +0900

    delete useless code

--- btn.js
+++ btn.js
@@ -1,9 +1,6 @@
 // const click = document.querySelector(".push_button");
 // const clicks = document.querySelector(".stop_button");
 
-
-
-
 var inter;
 var inter2;
 
@@ -31,38 +28,3 @@ function stop_click(){
     clearInterval(inter2)
     document.body.style.backgroundColor = "white"
 }
-// =======
-
-//     setInterval(function changecolor_click(){
-//         document.bgColor = "aqua";
-//         console.log("blue");
-//     },500)
-//     setInterval(function changecolor_click(){
-//             document.bgColor = "pink";
-//             console.log("red");   
-//     },800)
-    
-// }    
-
-
-// function stop_click() {
-//     clearInterval();
-//     alert("dd");
-// >>>>>>> Stashed changes
-// }
-
-
-// function stop_click(){
-//     clearInterval(inter)
-//     clearInterval(inter2)
-// }
-
-
-
-// setInterval(function changecolor_click(){
-//     document.bgColor = "aqua";
-// },500)
-
-// setInterval(function changecolor_click(){
-//     document.bgColor = "pink";
-// },700)
