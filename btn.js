6a4f27dfe975633cd239759e663b80af301f28ae
lhs0698 <104623873+lhs0698@users.noreply.github.com>
2022-06-09 20:13:23 +0900

    login add

--- btn.js
+++ btn.js
@@ -1,15 +1,32 @@
 let inter;
-let inter2;
 // 전역변수
+let isBlink = true; //toggle 변수
+let speed = 1000;
+
+let pushBtn = document.querySelector('.push_button');
+pushBtn.addEventListener('click', function(e) {
+    changecolor_click()
+})
+
+
+let stopBtn = document.querySelector('.stop_button');
+stopBtn.addEventListener('click', function(e) {
+    stop_click()
+})
+
 
 function changecolor_click(){
 
-    inter = setInterval(function color(){
-        document.body.style.backgroundColor = "aqua"
-    },500);
-    inter2 = setInterval(function color(){
-        document.body.style.backgroundColor = "pink"
-    },700);
+    if(!inter) {
+        inter = setInterval(function () {
+            if(isBlink) {
+                document.body.style.backgroundColor = "aqua"
+            } else {
+                document.body.style.backgroundColor = "pink"
+            }
+            isBlink = !isBlink
+        }, speed / 2)
+    }
 
     // let textInput = document.getElementById('setColor');
     // let colorValue = textInput.value;
@@ -19,42 +36,104 @@ function changecolor_click(){
 } 
 // push 버튼 이벤트
 
-function getInter(){
-    return [inter, inter2];
-}
+
 
 function stop_click(){
-    let inters = getInter();
-
-    clearInterval(inters[0]);
-    clearInterval(inters[1]);
+    clearInterval(inter);
+    inter = null;
     document.body.style.backgroundColor = "white"
 }
 // stop 버튼 이벤트
 
 // today_button
-function today_click(){
-    const time = new Date();
-    const year = time.getFullYear();
-    const month = time.getMonth()+1;
-    const date = time.getDate();
-    const hour = time.getHours();
-    const minute = time.getMinutes();
-    const second = time.getSeconds();
+// function today_click(){
+//     const time = new Date();
+//     const year = time.getFullYear();
+//     const month = time.getMonth()+1;
+//     const date = time.getDate();
+//     const hour = time.getHours();
+//     const minute = time.getMinutes();
+//     const second = time.getSeconds();
 
-    let timer = [year,month,date,hour,minute,second];
+//     let timer = [year,month,date,hour,minute,second];
     
     // alert(timer);
     // console.log(timer[2]);
 
-    let curtime = timer[4];
-    console.log(curtime);
+    // let curtime = timer[5];
+    // console.log(curtime);
 
-    if (curtime >= 20){
-        document.body.style.backgroundColor = "yellow"
-    }else {
-        document.body.style.backgroundColor = "green"
+    // if (curtime >= 30){
+    //     document.body.style.backgroundColor = "yellow"
+    // }else {
+    //     document.body.style.backgroundColor = "green"
+    // }
+// }
+
+// let type;
+// let loginBtn = document.querySelector('#login');
+// loginBtn.addEventListener('click', function(event) {
+//     let myInfo = {
+//         name: '현수',
+//         nickName: '해태',
+//         age: '26',
+//         addr: {
+//             dong: '시흥',
+//             si: '서울시',
+//             gu: '서대문구'
+//         }
+//     }
+
+//     let input = document.querySelector('#login_ID').value; //사용자입력
+
+//     // 비구조화 할당
+//     let { name, nickName, age, addr: { dong, si, gu } } = myInfo;
+
+//     if(input === name) {
+//         // document.body.style.backgroundColor = "yellow"
+//         type = 'same'
+//     } else {
+//         // document.body.style.backgroundColor = "green"
+//         type = 'diff'
+//     }
+//     let mess = (type === 'same') ? '<h1>일치합니다</h1>' : '<h1>틀렸습니다.</h1>'
+//     let messEle = document.querySelector('#mess');
+//     messEle.innerHTML = mess;
+// })
+
+let type;
+let loginBtn = document.querySelector("#login");
+
+loginBtn.addEventListener("click", function(){
+    let LOGIN = {
+        ID : "alta",
+        PW : "alta0204",
     }
-}
-// today_button
+    let newID = document.querySelector('#login_ID').value;
+    let newPW = document.querySelector("#login_PW").value;
+    // console.log(LOGIN);
+
+    let {ID: oldID} = LOGIN;
+    if(oldID === newID){
+        console.log("login!");
+    }else {
+        console.log("fail!");
+    }
+    
+    let {PW : oldPW} = LOGIN;
+    if(oldPW === newPW){
+        console.log("login!!");
+    }else{
+        console.log("fail!!")
+    }
+
+
+    // if (ID === LOGIN.ID){
+    //     let div = document.createElement('div');
+    //     div.innerHTML = '<h1>heee</h1>'
+    //     document.body.append(div);
+    // }
+})
+
+
 
